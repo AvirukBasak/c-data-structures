6063f41b7a84fcb47a0aa67bb99a3650fbb6cce0
AvirukBasak <avirukbasak@gmail.com>
2022-05-29 23:40:15 +0530

    updated main.c

--- src/llist/main.c
+++ src/llist/main.c
@@ -1,6 +1,11 @@
 # include <stdio.h>
 # include "llist.h"
 
+void callback (int64_t i, int64_t e)
+{
+    printf ("i = %ld: %ld\n", i, e);
+}
+
 int main ()
 {
     llist llst = new_llist ();
@@ -14,14 +19,14 @@ int main ()
     llist_append (llst, 47);
     llist_append (llst, 36);
 
-    printf ("Before set: ");
-    llist_print (llst);
+    printf ("Before set:\n");
+    llist_foreach (llst, callback);
 
     printf ("Value at i4 = %ld\n", llist_get (llst, 4));
     llist_set (llst, 4, 474);
 
-    printf ("After set: ");
-    llist_print (llst);
+    printf ("After set:\n");
+    llist_foreach (llst, callback);
 
     llist_delete (&llst);
     return 0;
